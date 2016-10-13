5cfb017cf3917c70980cf3fce5d4278a4a3d6b8a
Juan Carlos Coral <govindasd@gmail.com>
2016-10-13 18:55:55 -0300

    Adicionando items a seção 'pasionfruit'

--- build/javascripts/all.js
+++ build/javascripts/all.js
@@ -1,1 +1,40 @@
 // This is where it all goes :)
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+function setTopFeaturedHeight(){
+	var featBgImageHeight= $('.fundo-top-feature').height();
+	$('.top-featured').css('height', featBgImageHeight + 'px');
+	$('.passionfruit').css('margin-top', '-' + (featBgImageHeight / 17) + 'px');
+}
+
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+$(function() {
+	setTopFeaturedHeight();
+});
+
+$(document).ready(function(){
+	var oldWidth = $(this).width();
+		 var id ;
+		 //a executar no re-dimensionamento da janela de visualização
+		 $(window).resize(function(){
+			 clearTimeout(id);
+			 
+			 id = setTimeout(function() {
+		    														var width = $(this).width();
+																		if (width != oldWidth) {
+																    	oldWidth = width;
+																    	setTopFeaturedHeight();
+																    }
+																	}, 500
+											);
+		 });
+
+});
